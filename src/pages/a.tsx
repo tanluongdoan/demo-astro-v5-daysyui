2f591f02d6ceea2cdbb5998bd512105154c74c57
Lượng lượng <tanluong.doan@gmail.com>
2025-03-08 17:25:48 +0700

    fix

---
+++ src/pages/a.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const A = () => {
+  return (
+    <div>
+      <div className="tooltip tooltip-open" data-tip="hello">
+        <button className="btn">Force open</button>
+      </div>
+      <div className="flex " >
+        heello
+      </div>
+    </div>
+  );
+};
+
+export default A;
